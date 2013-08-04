87e5ef6f6ad79c2309966414f272f1357cadf91c
Faruk Celik <farukca@yahoo.com>
2013-08-04 13:57:14 +0300

    JS files added to precompile on deploy

--- lib/blogger/engine.rb
+++ lib/blogger/engine.rb
@@ -1,5 +1,10 @@
 module Blogger
   class Engine < ::Rails::Engine
     isolate_namespace Blogger
+
+    initializer "blogger.assets.precompile" do |app|
+      app.config.assets.precompile += %w(wysihtml5-advanced.js blogger/wysihtml5-0.4.0pre.min.js)
+    end
+
   end
 end
